bef52b61d0162bb58e201b99f0e045556f5ec81d
henriquepaulalima <henrique.lima@fluna.io>
2023-03-12 01:12:00 -0300

    Add images

---
+++ src/index.d.ts
@@ -0,0 +1,19 @@
+declare module "*.png" {
+  const path: any;
+  export = path;
+}
+
+declare module "*.jpg" {
+  const path: any;
+  export = path;
+};
+
+declare module "*.svg" {
+  const path: any;
+  export = path;
+};
+
+declare module "*.scss" {
+  const path: any;
+  export = path;
+};
