811e1747e01407a5f84f24c0361dcb9afc13fc20
henriquepaulalima <henrique.lima@fluna.io>
2023-03-23 18:19:48 -0300

    Create Banner component

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,36 @@
+import styles from './Banner.module.scss';
+import bannerMobile from 'assets/imgs/banner_mobile.jpg';
+import bannerTablet from 'assets/imgs/banner_tablet.jpg';
+import bannerDesktop from 'assets/imgs/banner_desktop.jpg';
+import { useEffect, useState } from 'react';
+
+export default function Banner() {
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  const [currentBackground, setCurrentBackground] = useState('');
+
+  useEffect(() => {
+    setScreenWidth(window.innerWidth);
+    getCurrentBackground(screenWidth)
+  });
+
+  function getCurrentBackground(width: number) {
+    if (width < 768) {
+      setCurrentBackground(bannerMobile);
+    } else if (width >= 768 && width < 1023) {
+      setCurrentBackground(bannerTablet)
+    } else if (width > 1023) {
+      setCurrentBackground(bannerDesktop);
+    } else {
+      throw new Error('Invalid screen width value');
+    }
+  }
+
+  return (
+    <section className={styles.banner__container}>
+      <img src={currentBackground} alt="Imagem do banner" />
+      <div className={styles.banner__text}>
+        <h1>Boas-vindas ao<br/>#CodeChella2023!</h1>
+      </div>
+    </section>
+  );
+}
