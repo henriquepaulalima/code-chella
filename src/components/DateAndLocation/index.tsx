2f8a665236123b7c095ef513be1678e9f6769c70
henriquepaulalima <henrique.p.lima0@gmail.com>
2023-04-16 18:20:27 -0300

    Modularize DataAndLocation component

---
+++ src/components/DateAndLocation/index.tsx
@@ -0,0 +1,16 @@
+import Button from 'components/Button';
+import styles from './DateAndLocation.module.scss';
+import date_and_location_img from 'assets/imgs/date_and_location.png'
+
+export default function DateAndLocation() {
+  return (
+    <section className={styles.date_and_location__container}>
+        <img className={styles.date_and_location__img} src={date_and_location_img} alt="Imagem da seção de datas e locatização" />
+        <div className={styles.info_container}>
+          <h2 className={styles.date_and_location__h2}>&lt; 11 e 12 de Março &gt;<br />Aluródromo de São Paulo</h2>
+          <p className={styles.date_and_location__p}>Hora de programar nossa memória com novas lembranças! Uma nova experiência sobre música, linguagens e, claro, tecnologia! Somos um festival diverso, com vários artistas e referências. Divirta-se!</p>
+          <Button content='Comprar ingresso!' ticketIcon={true}></Button>
+        </div>
+      </section>
+  );
+}
--- src/views/Main/index.tsx
+++ src/views/Main/index.tsx
@@ -1,20 +1,11 @@
-import styles from './Main.module.scss';
 import Banner from "components/Banner";
-import date_and_location_img from 'assets/imgs/date_and_location.png';
-import Button from 'components/Button';
+import DateAndLocation from "components/DateAndLocation";
 
 export default function Main() {
   return (
     <main>
       <Banner />
-      <section className={styles.date_and_location__container}>
-        <img className={styles.date_and_location__img} src={date_and_location_img} alt="Imagem da seção de datas e locatização" />
-        <div className={styles.info_container}>
-          <h2 className={styles.date_and_location__h2}>&lt; 11 e 12 de Março &gt;<br />Aluródromo de São Paulo</h2>
-          <p className={styles.date_and_location__p}>Hora de programar nossa memória com novas lembranças! Uma nova experiência sobre música, linguagens e, claro, tecnologia! Somos um festival diverso, com vários artistas e referências. Divirta-se!</p>
-          <Button content='Comprar ingresso!' ticketIcon={true}></Button>
-        </div>
-      </section>
+      <DateAndLocation />
     </main>
   );
 }
