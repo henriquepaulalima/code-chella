33b928bc010760b71d8770424d3d0e18e278a42e
henriquepaulalima <henrique.lima@fluna.io>
2023-05-27 23:35:35 -0300

    Fix Main component background

--- src/views/Main/index.tsx
+++ src/views/Main/index.tsx
@@ -1,10 +1,11 @@
 import Attractions from "views/Main/Attractions";
 import DateAndLocation from "./DateAndLocation";
 import MainBanner from "./MainBanner";
+import styles from './Main.module.scss';
 
 export default function Main() {
   return (
-    <main>
+    <main className={styles.main__container}>
       <MainBanner />
       <DateAndLocation />
       <Attractions />
