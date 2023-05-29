d3535df071039d14c7e9d915ba846397a25f0ec7
henriquepaulalima <henrique.lima@fluna.io>
2023-05-28 21:45:20 -0300

    Add background to Sectors component

--- src/views/Sectors/Sectors.tsx
+++ src/views/Sectors/Sectors.tsx
@@ -1,9 +1,12 @@
 import SectorsBanner from "./SectorsBanner/SectorsBanner";
+import SectorsStage from "./SectorsStage/SectorsStage";
+import styles from './Sectors.module.scss';
 
 export default function Sectors() {
   return (
-    <section>
+    <section className={styles.sectors__container}>
       <SectorsBanner />
+      <SectorsStage />
     </section>
   );
 }
