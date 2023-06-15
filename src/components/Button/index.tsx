ce320429cb5972392141483ee0873f36d8ea1662
henriquepaulalima <henrique.lima@fluna.io>
2023-06-15 18:24:04 -0300

    Add name, email, cpf and birthday validations

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,4 +1,3 @@
-import { NavigateFunction } from 'react-router-dom';
 import styles from './Button.module.scss';
 import ticket_icon from 'assets/imgs/ticket_icon.png';
 
@@ -6,11 +5,13 @@ type Props = {
   children: string,
   ticketIcon?: boolean,
   onClick?: any,
+  disabled?: boolean,
+  type?: "button" | "submit" | "reset" | undefined,
 }
 
-export default function Button({children, ticketIcon, onClick, ...props}: Props) {
+export default function Button({children, ticketIcon, onClick, disabled, type, ...props}: Props) {
   return (
-    <button onClick={onClick} className={styles.button_container}>
+    <button onClick={onClick} className={styles.button_container} disabled={disabled} type={type}>
       <p {...props}>{children}</p>
       {ticketIcon && (
         <img src={ticket_icon} alt="Ícone de ingresso" />
--- src/views/BuyTicket/BuyTicketForm/BuyTicketForm.tsx
+++ src/views/BuyTicket/BuyTicketForm/BuyTicketForm.tsx
@@ -1,12 +1,105 @@
 import Button from 'components/Button';
 import styles from './BuyTicketForm.module.scss';
 import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
+import classNames from 'classnames';
+import cpfMask from 'utils/cpfMask';
 
 export default function BuyTicketForm() {
   const navigate = useNavigate();
+  const [name, setName] = useState('');
+  const [isNameValid, setIsNameValid] = useState<boolean | null>(null);
+  const [email, setEmail] = useState('');
+  const [isEmailValid, setIsEmailValid] = useState<boolean | null>(null);
+  const [cpf, setCPF] = useState('');
+  const [isCPFValid, setIsCPFValid] = useState<boolean | null>(null);
+  const [birthdate, setBirthdate] = useState<any>(0);
+  const [isBirthdatValid, setIsBirthdatValid] = useState<boolean | null>(null);
+  const [isSubmitValid, setIsSubmitValid] = useState<boolean | null>(true);
 
-  const handleSubmit = () => {
-    navigate('/ticket');
+  const handleNameInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  }
+
+  const handleEmailInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  }
+
+  const handleCPFInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCPF(cpfMask(event.target.value));
+  }
+
+  const handleBirthdateInputChange = (event: React.ChangeEvent<HTMLDataElement>) => {
+    setBirthdate(event.target.value);
+  }
+
+  const verifyName = () => {    
+    const nameRegex = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u;
+
+    if (!nameRegex.test(name) || name === '') {
+      setIsNameValid(false);
+    } else {
+      setIsNameValid(true);
+    }
+
+    console.log(name, isNameValid);
+    
+  }
+
+  const verifyEmail = () => {
+    const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (!emailRegex.test(email) || email === '') {
+      setIsEmailValid(false);
+    } else {
+      setIsEmailValid(true);
+    }
+
+    console.log(email, isEmailValid);
+    
+  }
+
+  const verifyCPF = () => {
+    const cpfRegex = /^\d{3}\.\d{3}\.\d{3}\-\d{2}$/;
+
+    if (!cpfRegex.test(cpf) || cpf === '') {
+      setIsCPFValid(false);
+    } else {
+      setIsCPFValid(true);
+    }
+
+    console.log(cpf, isCPFValid);
+    
+  }
+
+  const verifyBirthday = () => {
+    if (birthdate === 0) {
+      setIsBirthdatValid(false);
+    } else {
+      setIsBirthdatValid(true);
+    }
+
+    console.log(birthdate, isBirthdatValid);
+    
+  }
+
+  const handleSubmit = (event: React.ChangeEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    verifyName();
+    verifyEmail();
+    verifyCPF();
+    verifyBirthday();
+
+    if (isNameValid && isEmailValid && isCPFValid && isBirthdatValid) {
+      setIsSubmitValid(true);
+      navigate('/ticket');
+    } else {
+      setIsSubmitValid(false);
+    }
+
+    console.log(isSubmitValid);
+    
   }
 
   return (
@@ -15,23 +108,67 @@ export default function BuyTicketForm() {
       <form onSubmit={handleSubmit} className={styles.buy_ticket_form__form}>
         <label className={styles.buy_ticket_form_label__container}>
           <p className={styles.buy_ticket_form_label__text}>Nome Completo:</p>
-          <input className={styles.buy_ticket_form_label__input} type="text" name="name" id="name" />
+          <input
+            className={classNames({
+              [styles.buy_ticket_form_label__input]: true,
+              [styles.inputError]: isNameValid === false,
+            })}
+            onChange={handleNameInputChange}
+            onBlur={verifyName}
+            value={name}
+            type="text"
+            name="name"
+            id="name"
+          />
         </label>
         <label className={styles.buy_ticket_form_label__container}>
           <p className={styles.buy_ticket_form_label__text}>Email:</p>
-          <input className={styles.buy_ticket_form_label__input} type="email" name="email" id="email" />
+          <input
+            className={classNames({
+              [styles.buy_ticket_form_label__input]: true,
+              [styles.inputError]: isEmailValid === false,
+            })}
+            onChange={handleEmailInputChange}
+            onBlur={verifyEmail}
+            value={email}
+            type="email"
+            name="email"
+            id="email"
+          />
         </label>
         <div className={styles.buy_ticket_form_double_label__container}>
           <label className={styles.buy_ticket_form_label__container}>
             <p className={styles.buy_ticket_form_label__text}>CPF</p>
-            <input className={styles.buy_ticket_form_label__input} type="number" name="cpf" id="cpf" />
+            <input
+              className={classNames({
+                [styles.buy_ticket_form_label__input]: true,
+                [styles.inputError]: isCPFValid === false,
+              })}
+              onChange={handleCPFInputChange}
+              onBlur={verifyCPF}
+              value={cpf}
+              type="text"
+              name="cpf"
+              id="cpf"
+            />
           </label>
           <label className={styles.buy_ticket_form_label__container}>
             <p className={styles.buy_ticket_form_label__text}>Data de nascimento</p>
-            <input className={styles.buy_ticket_form_label__input} type="date" name="birthday" id="birthday" />
+            <input
+              className={classNames({
+                [styles.buy_ticket_form_label__input]: true,
+                [styles.inputError]: isBirthdatValid === false,
+              })}
+              onChange={handleBirthdateInputChange}
+              onBlur={verifyBirthday}
+              value={birthdate}
+              type="date"
+              name="birthday"
+              id="birthday"
+            />
           </label>
         </div>
-        <Button >Avançar</Button>
+        <Button type="submit">Avançar</Button>
       </form>
     </div>
   );
